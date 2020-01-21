9751baebcefd4929f18aaa6b21ce191fc93885c9
Adam-Xi <321041835@qq.com>
2020-01-22 00:42:22 +0800

    圆的面积

---
+++ TheAreaOfCircle.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+    int radius;
+    double result, PI;
+    cin >> radius;
+    PI = atan(1.0) * 4;
+    result = PI * radius * radius;
+    printf("%.7lf", result);
+
+    return 0;
+}
+
